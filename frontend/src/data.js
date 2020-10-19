6038fca3bc24963b78b36ee5c400ddecffd3adec
ngocdu <ngocdu171@gmail.com>
2020-10-19 10:41:34 +0300

    fecth data

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/image1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/image1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/image1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/image1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/image1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        }
+    ]
+}
