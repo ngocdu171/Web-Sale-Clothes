2d0d70ececa5db0b26d93181603f081129bfabd5
ngocdu <ngocdu171@gmail.com>
2020-10-20 21:58:03 +0300

    create 2 component HomeScreen, ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import data from './data';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import HomeScreen from './components/HomeScreen';
+import ProductScreen from './components/ProductScreen';
 
 function App() {
     const openMenu = () =>{
@@ -10,52 +12,43 @@ function App() {
         document.querySelector(".sidebar").classList.remove("open");
     }
     return (
-        <div className="grid-container">
-            <header className="header">
-                <div className="brand">
-                    <button onClick= {openMenu}>
-                        ☰
-                    </button>
-                    <a href="index.html">Amazona</a>
-                </div>
-                <div className="header-link">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>Shopping Categories</h3>
-                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                <ul>
-                    <li>
-                        <a href="index.html">Pants</a>
-                    </li>
-                    <li>
-                        <a href="index.html">Shirts</a>
-                    </li>
-                </ul>
-            </aside>
-            <main className="main">
-                <div className="content">
-                    <ul className="products">
-                        {data.products.map(product =>
-                            <li>
-                                <div className="product">
-                                <img className="product-image" src={product.image} alt="product" />
-                                <div className="product-name"><a href="product.html">{product.name}</a></div>
-                                <div className="product-brand">{product.brand}</div>
-                                <div className="product-price">${product.price}</div>
-                                <div className="product-rating">{product.rating} Stars({product.numReviews} reviews)</div>
-                                </div>
-                            </li>
-                        )}
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick= {openMenu}>
+                            ☰
+                        </button>
+                        <Link to="/" >Amazona</Link>
+                    </div>
+                    <div className="header-link">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign In</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul>
+                        <li>
+                            <a href="index.html">Pants</a>
+                        </li>
+                        <li>
+                            <a href="index.html">Shirts</a>
+                        </li>
                     </ul>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <Route path="/product/:id" component={ProductScreen} />
+                        <Route path="/" exact={true} component={HomeScreen} />
+                    </div>
+                </main>
+                    <footer className="footer">
+                        All right reserved.
+                    </footer>
                 </div>
-            </main>
-                <footer className="footer">
-                    All right reserved.
-                </footer>
-            </div>
+            </BrowserRouter>
         );
     }
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,49 +1,64 @@
 export default {
     products: [
         {
+            _id: '1',
             name: 'Slim Shirt',
             category: 'Shirts',
             image: '/images/image1.jpg',
             price: 60,
             brand: 'Nike',
             rating: 4.5,
-            numReviews: 10
+            numReviews: 10,
+            description: "Nice Shirt",
+            status: "In Stock"
         },
         {
+            _id: '2',
             name: 'Fit Shirt',
             category: 'Shirts',
             image: '/images/image1.jpg',
             price: 50,
             brand: 'Nike',
             rating: 4.2,
-            numReviews: 5
+            numReviews: 5,
+            description: "Nice Shirt",
+            status: "Out of Stock"
         },
         {
+            _id: '3',
             name: 'Best Pants',
             category: 'Pants',
             image: '/images/image1.jpg',
             price: 70,
             brand: 'Nike',
             rating: 4.5,
-            numReviews: 8
+            numReviews: 8,
+            description: "Nice Pant",
+            status: "In Stock"
         },
         {
+            _id: '4',
             name: 'Best Pants',
             category: 'Pants',
             image: '/images/image1.jpg',
             price: 70,
             brand: 'Nike',
             rating: 4.5,
-            numReviews: 8
+            numReviews: 8,
+            description: "Nice Pant",
+            status: "In Stock"
         },
         {
+            _id: '5',
             name: 'Best Pants',
             category: 'Pants',
             image: '/images/image1.jpg',
             price: 70,
             brand: 'Nike',
             rating: 4.5,
-            numReviews: 8
+            numReviews: 8,
+            description: "Nice Pant",
+            status: "Out of Stock"
         }
     ]
 }
